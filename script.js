3ae4ffaa3d2b688a5f268869c02b16c7c13ec6c0
Emma Bian <162594093+emma-bian@users.noreply.github.com>
2026-06-27 20:31:12 -0400

    add js to user input

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert("connected!")
+const button = document.getElementById("search-btn")
+const input = document.getElementById("ingredient-input")
+const results = document.getElementById("recipe-results")
+
+function handleSearch() {
+    const userInput = input.value.trim() //value reads live state of box
+
+    if (userInput === "") return
+
+    results.innerHTML = userInput //replaces html in results section
+}
+
+button.addEventListener("click", handleSearch)
+
+input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") handleSearch()
+})
