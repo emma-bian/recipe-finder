9d6eaac8251b95d40f4faef13b3151f4af581567
Emma Bian <162594093+emma-bian@users.noreply.github.com>
2026-06-27 20:51:07 -0400

    connect api

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ button.addEventListener("click", handleSearch)
 input.addEventListener("keydown", (event) => {
     if (event.key === "Enter") handleSearch()
 })
+
+async function handleSearch() {
+    const userInput = input.value.trim();
+    if (userInput === "") return;
+
+    try {
+        const response = await fetch(
+            `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${userInput}&number=10&apiKey=${RECIPE_FINDER_API_KEY}`
+        );
+        const data = await response.json();
+        console.log("Content-Type Header:", response.headers.get('content-type'));
+        // console.log(data); // see what comes back first
+    } catch (error) {
+        console.error("Something went wrong:", error);
+    }
+}
